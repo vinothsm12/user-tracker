677e86afc2afdc723d2503769e5f38521d00722d
vinothsm12 <vinoth.sm12@gmail.com>
2025-04-01 18:45:13 +0530

    create an API to create issue

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -146,7 +146,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
@@ -156,6 +156,7 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
